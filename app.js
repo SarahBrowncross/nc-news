21d74ce8f4e6cdd8378eda1168ed11fd520e40cd
SarahBrowncross <sarah.browncross@gmail.com>
2020-09-25 16:34:40 +0100

    changes to listen file

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ app.use(send404UserError);
 app.use(handle400Error);
 app.use(send500Error);
 
-const { PORT = 9090 } = process.env;
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
 
 module.exports = app;
